b33c7183c5e66f824ceb7aa8dae7a923766cffce
domephant <66464989+domephant@users.noreply.github.com>
2022-01-04 00:00:53 +0100

    Added basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -14,21 +14,12 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const DatabaseTextScreen(),
@@ -41,25 +32,19 @@ class DatabaseTextScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    CollectionReference todos = FirebaseFirestore.instance.collection('to-dos');
-
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: FutureBuilder(
-          future: todos.doc().get(),
-          builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              print(snapshot.data!.data());
-              return Text("Daten bekommen");
-            }
-            if (snapshot.hasError) {
-              return Text("Error");
-            }
-            return Container();
-          },
-        ),
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.settings))
+        ],
+        title: const Text("To-Do"),
       ),
+      drawer: const Drawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      body: ListView(),
     );
   }
 }
